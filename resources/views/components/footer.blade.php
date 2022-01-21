d77cc9d24134ed3502a25cc3467f2e8bbb09899e
Giuseppe0197 <faro.g@hotmail.it>
2022-01-21 17:11:23 +0100

    add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,93 @@
+<footer>
+
+    <div class="container-footer">
+
+        <div class="footer-links">
+
+            <div class="list-container">
+    
+                <div class="dc-list">
+                    <h4>Dc Comics</h4>
+        
+                    <ul class="list">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+        
+                    <h4>Shop</h4>
+        
+                    <ul class="list">
+                        <li>Shop DC</li>
+                        <li>Shop Dc Collectibles</li>
+                    </ul>
+                </div>
+        
+                <div class="dc-list">
+                    <h4>DC</h4>
+        
+                    <ul class="list">
+                        <li>Terms of use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscription</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+        
+                <div class="dc-list">
+                    <h4>Sites</h4>
+        
+                    <ul class="list">
+                        <li>DC</li>
+                        <li>MAD Magazines</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+    
+            <div class="imgcontainer">
+                <img src="https://cdnb.artstation.com/p/assets/images/images/029/700/321/large/semrram-gonzalez-dc-logo.jpg?1598376808" alt="">
+            </div>
+    
+        </div>
+
+    </div>
+
+    <div class="social">
+        
+        <div class="elements">
+
+            <div class="sign">
+
+                <button class="sign-up">Sign-up now!</button>
+
+            </div>
+
+            <div class="icons">
+
+                <span>Follow us</span>
+
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="">
+                <img src="https://www.savepng.com/file/thumb/2020-06/2-2-twitter-png-file-thumb.png" alt="">
+                <img src="https://youtubevisualizzazioni.com/images/YouTube_Play.png" alt="">
+                <img src="https://i.pinimg.com/280x280_RS/dd/59/57/dd5957c4a0106bde7cd303c7ce835762.jpg" alt="">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhv2dLIhZntdlUSudvltNgnfJ7KpA6kPtVw&usqp=CAU" alt="">
+
+            </div>
+        </div>
+    </div>
+    
+</footer>
