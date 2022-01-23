b89d37635839112ae24156f796bd2976ac58e7d6
Giuseppe0197 <faro.g@hotmail.it>
2022-01-23 15:49:10 +0100

    add footer images

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -80,11 +80,11 @@
 
                 <span>Follow us</span>
 
-                <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="">
-                <img src="https://www.savepng.com/file/thumb/2020-06/2-2-twitter-png-file-thumb.png" alt="">
-                <img src="https://youtubevisualizzazioni.com/images/YouTube_Play.png" alt="">
-                <img src="https://i.pinimg.com/280x280_RS/dd/59/57/dd5957c4a0106bde7cd303c7ce835762.jpg" alt="">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLhv2dLIhZntdlUSudvltNgnfJ7KpA6kPtVw&usqp=CAU" alt="">
+                <img src="img/footer-facebook.png" alt="">
+                <img src="img/footer-twitter.png" alt="">
+                <img src="img/footer-youtube.png" alt="">
+                <img src="img/footer-pinterest.png" alt="">
+                <img src="img/footer-periscope.png" alt="">
 
             </div>
         </div>
